ebb4ef52912b63fcac99de5e03c8ff17b334c6eb
Xoel López Barata <xoelop@gmail.com>
2020-01-30 18:08:50 +0100

    add readme

---
+++ data/like_example.js
@@ -0,0 +1,19 @@
+window.YTD.like.part0 = [ {
+  "like" : {
+    "tweetId" : "1220047117537947661",
+    "fullText" : "Here's how Ecudaor disposes of their litter.\n\nGreta going to be visiting Quito any time soon, or just lecturing the U.S. &amp; Europe?\n\nhttps://t.co/AiXwhXUuM3",
+    "expandedUrl" : "https://twitter.com/i/web/status/1220047117537947661"
+  }
+}, {
+  "like" : {
+    "tweetId" : "1220080725178888193",
+    "fullText" : "I’m answering questions on @IndieHackers for the next 30 minutes or so! Drop in to ask about salary negotiation, entrepreneurship, how I make decisions, or anything else! \n\nhttps://t.co/2zdJgluYWc",
+    "expandedUrl" : "https://twitter.com/i/web/status/1220080725178888193"
+  }
+}, {
+  "like" : {
+    "tweetId" : "1220246340510203904",
+    "fullText" : "people often are not aware that with @blockstream green wallet you can use a hardware wallet like @Ledger and @Trezor directly on android phones and tablets with a micro-USB otg cable. multisig 2fa+hww+mobile=\uD83D\uDD12https://t.co/1PqdURhhki https://t.co/JoeMh12tEk",
+    "expandedUrl" : "https://twitter.com/i/web/status/1220246340510203904"
+  }
+}]
