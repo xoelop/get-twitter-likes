fa38a377a5e4b9ac05fcf2e0e63f3e5c18fc9857
Xoel López Barata <xoelop@gmail.com>
2020-04-29 16:57:54 +0200

    Add Postgres support to write likes on SQL table

--- get_likes_to_csv.py
+++ get_likes_to_csv.py
@@ -1,8 +1,19 @@
 from src.core import get_all_statuses, create_df_statuses
 import pandas as pd
+import argparse
 
-print('Downloading likes detailed data from Twitter API')
-likes = get_all_statuses()
-df = create_df_statuses(likes)
-df.to_csv('data/likes.csv')
-print('Likes saved in data/likes.csv')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--o', '-output', help='Destination file', default='data/likes.csv')
+    parser.add_argument('--f', '-format', help='Format of specific columns of the csv: raw or gsheets', default='gsheets')
+    args = parser.parse_args()
+    output_file = args.o
+    output_format = args.f
+    print(args)
+
+    print('Downloading likes detailed data from Twitter API')
+    likes = get_all_statuses(output_format=output_format)
+    df = create_df_statuses(likes)
+    df.to_csv(output_file, index=False)
+    print(f'Likes saved in {output_file}')
